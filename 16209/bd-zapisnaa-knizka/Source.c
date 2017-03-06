5a55e297b444e692c4ea7946ded2abca88978ffc
BanyRule <banyrule@gmail.com>
2017-03-07 00:50:42 +0700

    Update Source.c

--- 16209/bd-zapisnaa-knizka/Source.c
+++ 16209/bd-zapisnaa-knizka/Source.c
@@ -17,30 +17,75 @@ int main(int argc, char *argv[])
 
 	do
 	{
-		printf("HERE... ");
 		char input_cmd[INPUT_SIZE];
 		fgets(input_cmd, INPUT_SIZE, stdin);
 		
 		if (strcmp(input_cmd, "exit\n") == 0)
 			break;
+		else if (strcmp(input_cmd, "add\n") == 0)
+		{
+			printf("NAME, TEAM, ELO >> ");
+			char *name = (char*)malloc(16);
+			char *team = (char*)malloc(16);
+			int elo = 0;
+			scanf("%s%s%d", name, team, &elo);
+			CONTEST_registration(&contest, name, team, elo);
+		}
+		else if (strcmp(input_cmd, "delete\n") == 0)
+		{
+			printf("NAME >> ");
+			char *name = (char*)malloc(16);
+			scanf("%s", name);
+			CONTEST_delete_player(&contest, name);
+		}
+		else if (strcmp(input_cmd, "printall\n") == 0)
+		{
+			CONTEST_show_players(contest);
+		}
+		else if (strcmp(input_cmd, "elo\n") == 0)
+		{
+			int elo;
+			printf("ELO, +/- ~ GT/LT >> ");
+			scanf("%d", &elo);
+			CONTEST_filter_by_elo(contest, elo);
+		}
+		else if (strcmp(input_cmd, "team\n") == 0)
+		{
+			char team[32];
+			printf("TEAM >> ");
+			scanf("%s", &team);
+			CONTEST_filter_by_team(contest, team);
+		}
+		else if (strcmp(input_cmd, "save\n") == 0)
+		{
+			char filename[32];
+			printf("FILENAME  >> ");
+			scanf("%s", &filename);
 
-		if (strcmp(input_cmd, "add\n") == 0)
-		{} 
+			FILE *db = fopen(filename, "w");
+			if (db == NULL)
+			{
+				printf("wrong path\n");
+				continue;
+			}
+			CONTEST_save(db, contest);
+			fclose(db);
+		}
+		else if (strcmp(input_cmd, "load\n") == 0)
+		{
+			char filename[32];
+			printf("FILENAME  >> ");
+			scanf("%s", &filename);
 
-		if (strcmp(input_cmd, "delete\n") == 0)
-		{}
-
-		if (strcmp(input_cmd, "printall\n") == 0)
-		{}
-
-		if (strcmp(input_cmd, "sort\n") == 0)
-		{}
-
-		if (strcmp(input_cmd, "save\n") == 0)
-		{}
-
-		if (strcmp(input_cmd, "load\n") == 0)
-		{}
+			FILE *db = fopen(filename, "r");
+			if (db == NULL)
+			{ 
+				printf("wrong path\n");
+				continue;
+			}
+			CONTEST_load(db, &contest);
+			fclose(db);
+		}
 
 	} while (inf_loop);
 
@@ -59,7 +104,7 @@ int main(int argc, char *argv[])
 	//CONTEST_save(db, QuakeCon2004);
 	//fclose(db);
 
-	//FILE *db = fopen("playerlist.txt", "r");
+	//FILE *db = fopen("QuakeCon.txt", "r");
 	//CONTEST_load(db, &QuakeCon2004);
 	//fclose(db);
 	//
