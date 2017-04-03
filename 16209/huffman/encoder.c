4489d615407f5d33f55795269e73ee41cd9af0a7
BanyRule <banyrule@gmail.com>
2017-04-03 16:39:35 +0700

    Update encoder.c

--- 16209/huffman/encoder.c
+++ 16209/huffman/encoder.c
@@ -23,32 +23,27 @@ typedef struct node_t {
 //  Функции для инициализации libpqueue...
 //
 
-static int
-cmp_pri(pqueue_pri_t next, pqueue_pri_t curr)
+static int cmp_pri(pqueue_pri_t next, pqueue_pri_t curr)
 {
-	return (next < curr);
+	return (next > curr);
 }
 
-static pqueue_pri_t
-get_pri(void *a)
+static pqueue_pri_t get_pri(void *a)
 {
 	return ((node_t *)a)->pri;
 }
 
-static void
-set_pri(void *a, pqueue_pri_t pri)
+static void set_pri(void *a, pqueue_pri_t pri)
 {
 	((node_t *)a)->pri = pri;
 }
 
-static size_t
-get_pos(void *a)
+static size_t get_pos(void *a)
 {
 	return ((node_t *)a)->pos;
 }
 
-static void
-set_pos(void *a, size_t pos)
+static void set_pos(void *a, size_t pos)
 {
 	((node_t *)a)->pos = pos;
 }
@@ -58,11 +53,12 @@ set_pos(void *a, size_t pos)
 node_t * new_node(size_t freq, uint8_t c, node_t * left, node_t * right)
 {
 	node_t * n = (node_t *)malloc(sizeof(node_t));
+	n->val = c;
+	n->left = left, n->right = right;
 	if (freq)
-		n->val = c, n->pri = freq;
+		n->pri = freq;
 	else
 	{
-		n->left = left, n->right = right;
 		n->pri = left->pri + right->pri;
 	}
 
@@ -75,9 +71,9 @@ void fgetc_freq(char * file_name, size_t * freq)
 	if (fin == NULL) { return; }
 	_setmode(_fileno(fin), _O_BINARY);
 
-	uint8_t buff[1024];						// читать будем блоками по киловайту
+	uint8_t buff[1024];										// читать будем блоками по киловайту
 	size_t buff_size = sizeof(buff) / sizeof(*buff);		// но это не точно
-	size_t cur_size = 0;						// сколько смогли прочитать
+	size_t cur_size = 0;									// сколько смогли прочитать
 
 	
 	while ((cur_size = fread(buff, 1, buff_size, fin)) > 0)
@@ -92,8 +88,43 @@ void fgetc_freq(char * file_name, size_t * freq)
 }
 
 // обход дерева в глубину: 0 - left, 1 - right
-void build_code()
+// head - корень дерева
+// s - строка, в которую накапливается код символа, len - смещение ввода
+// code - здесь будут коды символов
+void build_code(node_t * head, uint8_t * code[], uint8_t * s, int len)
 {
+	if (!head)
+		return;
+
+	if (head->val)
+	{
+		s[len] = 0;
+		code[head->val] = malloc(sizeof(uint8_t) * len);
+		strcpy(code[head->val], s);
+		return;
+	}
+
+	s[len] = '0', build_code(head->left, code, s, len + 1);
+	s[len] = '1', build_code(head->right, code, s, len + 1);
+}
+
+// скобочная запись дерева (для отладки)
+void tree_print(node_t * root)
+{
+	if (!root)
+	{
+		putchar('x');
+		return;
+	}
+
+	putchar('(');
+	if (root->val)
+		putchar(root->val);
+	else
+		putchar('N');
+	tree_print(root->left);
+	tree_print(root->right);
+	putchar(')');
 }
 
 void encoding(char * input_file_name, char * output_file_name)
@@ -102,29 +133,31 @@ void encoding(char * input_file_name, char * output_file_name)
 	// TODO: вынести все это в отдельную ф-ию init()
 	//
 
-	//
 	// частота вхождения каждого символа
-	//
-
 	size_t freq[256] = { 0 };
 	fgetc_freq(input_file_name, freq);
 	
-	//
 	// построение кодирующего дерева
-	//
-
 	pqueue_t * pq = pqueue_init(256, cmp_pri, get_pri, set_pri, get_pos, set_pos);
 	for (size_t i = 0; i < 256; ++i)
 	{
 		if (freq[i])
 		{
-			pqueue_insert(pq, new_node(freq[i], (uint8_t)i, 0, 0));
+			pqueue_insert(pq, new_node(freq[i], (uint8_t)i, NULL, NULL));
 		}
 	}
+	// обьедиинять узлы, пока в очереди больше одного
 	while (pqueue_size(pq) >= 2)
 	{
-		pqueue_insert(pq, new_node(0, 0, (node_t *)pqueue_pop(pq), (node_t *)pqueue_pop(pq)));
+		pqueue_insert(pq, new_node(0, (uint8_t)0, (node_t *)pqueue_pop(pq), (node_t *)pqueue_pop(pq)));
 	}
 	
-	// build_code((node_t *)pqueue_peek(pq), ...);
+	// собраное дерево
+	tree_print((node_t *)pqueue_peek(pq)); putchar('\n');
+
+	uint8_t *code[256] = { 0 };
+	uint8_t s[16];
+	node_t * root = (node_t *)pqueue_peek(pq);
+	build_code(root, code, s, 0);
+
 }
